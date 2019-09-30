7e555e1ed5aca6e7924a43ca77d2eee1b1f5d360
Adrian Zdankiewicz <15159664+azdankiewicz@users.noreply.github.com>
2019-09-30 08:41:00 +0200

    Add ACK frame

--- src/main/java/com/sim/network/EthAdapter.java
+++ src/main/java/com/sim/network/EthAdapter.java
@@ -6,6 +6,7 @@ import desmoj.core.simulator.TimeSpan;
 import desmoj.core.simulator.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,8 +15,9 @@ import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class EthAdapter extends ExternalEvent {
-    private final boolean ENABLE_ACK = false;
+    private final boolean ENABLE_ACK = true;
 
     @Getter
     private final Queue<EthFrame> inAdapterQueue;
@@ -47,6 +49,77 @@ public class EthAdapter extends ExternalEvent {
         collisionDetected = false;
     }
 
+    @Override
+    public void eventRoutine() {
+        NetworkModel model = (NetworkModel) getModel();
+
+        if (!isCollisionDetected()) {
+            outMsgQueue(model);
+            adapterQueue(model);
+        }
+
+        inAdapterQueue(model);
+
+        schedule(new TimeSpan(1, TimeUnit.MICROSECONDS));
+    }
+
+    private void outMsgQueue(NetworkModel model) {
+        if (!outMsgQueue.isEmpty()) {
+
+            TCPMessage tcpMessage = outMsgQueue.first();
+            outMsgQueue.remove(tcpMessage);
+
+            EthFrame frame = createFrame(model, (tcpMessage.getName().contains("ACK") ? "ETH-ACK-Frame" : "ETH-Frame"));
+            frame.setDestAddress(tcpMessage.getDstAddress());
+            frame.setTcpMessage(tcpMessage);
+
+            outAdapterQueue.insert(frame);
+        }
+    }
+
+    private void adapterQueue(NetworkModel model) {
+        if (!outAdapterQueue.isEmpty()) {
+            EthFrame frame = outAdapterQueue.first();
+            outAdapterQueue.remove(frame);
+            model.getEthPendingBuffer().insert(frame);
+        }
+    }
+
+    private void inAdapterQueue(NetworkModel model) {
+        if (!inAdapterQueue.isEmpty()) {
+            EthFrame inFrame = inAdapterQueue.first();
+            inAdapterQueue.remove(inFrame);
+
+            sendTraceNote("FRAME-STOP " + inFrame.getName());
+            inFrame.getTcpMessage().setStopTransmission(presentTime());
+
+            if (ENABLE_ACK) {
+                String timeStamp = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss.SSS: ").format(new Date());
+
+                if (inFrame.getName().contains("ACK")) {
+                    log.info(timeStamp + "ACK recv: src=" + inFrame.getTcpMessage().getSrcAddress() + ", dst=" + inFrame.getTcpMessage().getDstAddress()
+                            + ", " + inFrame.getName() + ", adapter=" + getName());
+
+                    sendTraceNote("TCPMSG-ACK-RECV-TIME|" + inFrame.getTcpMessage().getTransmissionTime().toString() + "|" + getName());
+                } else {
+                    TCPMessage ackMsg = new TCPMessage(model, "ACK Message", true);
+                    ackMsg.setSrcAddress(inFrame.getTcpMessage().getDstAddress());
+                    ackMsg.setDstAddress(inFrame.getTcpMessage().getSrcAddress());
+
+                    outMsgQueue.insert(ackMsg);
+
+                    sendTraceNote("TCPMSG-ACK-SENT-TIME|" + inFrame.getTcpMessage().getTransmissionTime().toString() + "|" + getName());
+
+                    log.info(timeStamp + "ACK sent: src=" + ackMsg.getSrcAddress() + ", dst=" + ackMsg.getDstAddress()
+                            + ", " + ackMsg.getName() + ", adapter=" + getName());
+                }
+            }
+
+            sendTraceNote("TCPMSG-LEFT-TIME|" + inFrame.getTcpMessage().getTransmissionTime().toString() + "|" + inFrame.adapter.getName());
+            inMsgQueue(model);
+        }
+    }
+
     private EthFrame createFrame(NetworkModel model, String name) {
         List<EthAdapter> adapters = model.getEthAdapterList().stream()
                 .filter(x -> Objects.equals(getName(), x.getName())).collect(Collectors.toList());
@@ -55,72 +128,8 @@ public class EthAdapter extends ExternalEvent {
         return new EthFrame(model, name, true, adapters.stream().findFirst().orElse(null), presentTime());
     }
 
-    @Override
-    public void eventRoutine() {
-        NetworkModel model = (NetworkModel) getModel();
-
-        if (!isCollisionDetected()) {
-            if (!outMsgQueue.isEmpty()) {
-
-                TCPMessage tcpMessage = outMsgQueue.first();
-                outMsgQueue.remove(tcpMessage);
-
-                EthFrame frame = createFrame(model, "ETH-Frame");
-                frame.setDestAddress(tcpMessage.getDstAddress());
-                frame.setTcpMessage(tcpMessage);
-
-                outAdapterQueue.insert(frame);
-            }
-
-            if (!outAdapterQueue.isEmpty()) {
-                EthFrame frame = outAdapterQueue.first();
-                outAdapterQueue.remove(frame);
-                model.getEthPendingBuffer().insert(frame);
-            }
-        }
-
-        if (!inAdapterQueue.isEmpty()) {
-            EthFrame inFrame = inAdapterQueue.first();
-            inAdapterQueue.remove(inFrame);
-
-            if (ENABLE_ACK) {
-                inFrame.getTcpMessage().setStopTransmission(presentTime());
-                if (inFrame.getName().contains("ACK")) {
-                    String timeStamp = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss.SSS: ").format(new Date());
-
-                    System.err.println(timeStamp + "ACK recv: src=" + inFrame.getTcpMessage().getSrcAddress() + ", dst=" + inFrame.getTcpMessage().getDstAddress()
-                            + ", " + inFrame.getName() + ", adapter=" + getName());
-
-                    sendTraceNote("FRAME-ACK " + inFrame.getName());
-                    sendTraceNote("TCPMSG-ACK-TIME|" + inFrame.getTcpMessage().getTransmissionTime().toString() + "|" + getName());
-                } else {
-                    TCPMessage msg = new TCPMessage(model, "ACK Message", true);
-                    msg.setSrcAddress(inFrame.getTcpMessage().getDstAddress());
-                    msg.setDstAddress(inFrame.getTcpMessage().getSrcAddress());
-
-                    EthFrame ackFrame = createFrame(model, "ACK-Frame");
-                    ackFrame.setDestAddress(msg.getDstAddress());
-                    ackFrame.setTcpMessage(msg);
-
-                    outAdapterQueue.insert(ackFrame);
-
-                    sendTraceNote("FRAME-STOP " + inFrame.getName());
-
-                    String timeStamp = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss.SSS: ").format(new Date());
-                    System.err.println(timeStamp + "ACK sent: src=" + ackFrame.getTcpMessage().getSrcAddress() + ", dst=" + ackFrame.getTcpMessage().getDstAddress()
-                            + ", " + ackFrame.getName() + ", adapter=" + getName());
-                }
-            } else {
-                sendTraceNote("FRAME-STOP " + inFrame.getName());
-                inFrame.getTcpMessage().setStopTransmission(presentTime());
-            }
-
-            sendTraceNote("TCPMSG-LEFT-TIME|" + inFrame.getTcpMessage().getTransmissionTime().toString() + "|" + inFrame.adapter.getName());
-
-            TCPMessage inTCPMessage = new TCPMessage(model, "IN-TCP-Message", true);
-            inMsgQueue.insert(inTCPMessage);
-        }
-
-        schedule(new TimeSpan(1, TimeUnit.MICROSECONDS));
+    private void inMsgQueue(NetworkModel model) {
+        TCPMessage inTCPMessage = new TCPMessage(model, "IN-TCP-Message", true);
+        inMsgQueue.insert(inTCPMessage);
     }
 }
--- src/main/java/com/sim/network/bacnet/BacnetConverterEvent.java
+++ src/main/java/com/sim/network/bacnet/BacnetConverterEvent.java
@@ -2,30 +2,28 @@ package main.java.com.sim.network.bacnet;
 
 import desmoj.core.simulator.Event;
 import desmoj.core.simulator.Model;
-import co.paralleluniverse.fibers.SuspendExecution;
 import main.java.com.sim.network.NetworkModel;
 import main.java.com.sim.network.TCPMessage;
 
 public class BacnetConverterEvent extends Event<BacnetMessage> {
-
-    private BacnetMessage receivedMsg;
     private BacnetAdapter bacnetAdapter;
 
-    public BacnetConverterEvent(Model owner, String name, boolean showInTrace, BacnetAdapter rs232Adapter) {
+    BacnetConverterEvent(Model owner, String name, boolean showInTrace, BacnetAdapter rs232Adapter) {
         super(owner, name, showInTrace);
         this.bacnetAdapter = rs232Adapter;
     }
 
     @Override
-    public void eventRoutine(BacnetMessage bacnetMessage) throws SuspendExecution {
-        NetworkModel model = (NetworkModel)getModel();
-        this.receivedMsg = bacnetMessage;
+    public void eventRoutine(BacnetMessage bacnetMessage) {
+        NetworkModel model = (NetworkModel) getModel();
 
         TCPMessage tcpMessage = new TCPMessage(model, "Bacnet To TCP", true);
+
+        tcpMessage.setSrcAddress(this.bacnetAdapter.getEthAdapter().getAdapterAddress());
         tcpMessage.setDstAddress(bacnetMessage.getTcpDstAddress());
-        tcpMessage.setDstAddress(this.bacnetAdapter.getEthAdapter().getAdapterAddress());
-        if(this.bacnetAdapter.inBacnetAdapterQueue != null) {
-            this.bacnetAdapter.inBacnetAdapterQueue.insert(tcpMessage);
+
+        if (this.bacnetAdapter.getInBacnetAdapterQueue() != null) {
+            this.bacnetAdapter.getInBacnetAdapterQueue().insert(tcpMessage);
         }
     }
 }
